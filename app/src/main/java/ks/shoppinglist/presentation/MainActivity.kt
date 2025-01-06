c24f5d4004d7207a7e4a9259d939b15042b9b2ca
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2025-01-06 19:12:37 +0300

    initial commit

--- app/src/main/java/ks/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/ks/shoppinglist/presentation/MainActivity.kt
@@ -16,11 +16,21 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.shopList.observe(this) {
+            Log.d("Main Activity test", it.toString())
+            if (count == 0) {
+                count++
+                val item = it[0]
+                viewModel.changeEnableState(item)
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
