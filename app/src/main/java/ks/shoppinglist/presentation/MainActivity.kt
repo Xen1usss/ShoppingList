94790c9bb344781b5e905d7b1bbbc9e9f1c7b866
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2025-01-10 15:12:02 +0300

    make adapter

--- app/src/main/java/ks/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/ks/shoppinglist/presentation/MainActivity.kt
@@ -1,36 +1,30 @@
 package ks.shoppinglist.presentation
 
 import android.os.Bundle
-import android.text.Layout
-import android.util.Log
-import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import ks.shoppinglist.R
-import ks.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var llShopList: LinearLayout
     private var count = 0
+    private lateinit var adapter: ShopListAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        llShopList = findViewById(R.id.ll_layout_shop_list)
-        viewModel.shopList.observe(this) {
-            showList(it)
+        adapter = ShopListAdapter()
+        setContentView(R.layout.activity_main) //
+        setUpRecyclerView()
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java) //
+        viewModel.shopList.observe(this) { //
+            adapter.shopList = it
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -39,27 +33,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showList(list: List<ShopItem>) {
-        llShopList.removeAllViews()
-        for (shopItem in list) {
-            val layoutId = if (shopItem.enabled) {
-                R.layout.item_shop_enabled
-            } else {
-                R.layout.item_shop_disabled
-            }
-            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
-            val tvName = view.findViewById<TextView>(R.id.tv_name)
-            val tvCount = view.findViewById<TextView>(R.id.tv_count)
-            tvName.text = shopItem.name
-            tvCount.text = shopItem.count.toString()
-            view.setOnLongClickListener {
-                viewModel.changeEnableState(shopItem)
-                true
-            }
-            llShopList.addView(view)
-        }
+    private fun setUpRecyclerView() { //
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list) //
+        adapter = ShopListAdapter() //
+        rvShopList.adapter = adapter //
     }
-
 
 
 }
